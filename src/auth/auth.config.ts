561080db215278cf1752f6d376b35a3d5ea81a11
Andrew Russell <arustle@pm.me>
2020-05-11 21:31:36 -0400

    iiiiii

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -2,11 +2,19 @@
 // const apiId = 'rjxa11xk05'
 // export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 // export const apiEndpoint = `http://localhost:3000/dev`
-export const apiEndpoint = `https://api.measuringchange.com/dev`
+// export const apiEndpoint = `https://api.measuringchange.com/dev`
+//
+// export const AUTH_CONFIG = {
+//   clientId: 'jaGPHsP26hXndaxXddaLR4jN2wmqYsZZ',
+//   domain: 'dev-ch11m2eo.auth0.com',
+//   // redirectUri: 'http://localhost:3000/callback',
+//   redirectUri: 'https://measuringchange.com/callback',
+// }
+
+export const apiEndpoint = process.env.API_ENDPOINT;
 
 export const AUTH_CONFIG = {
-  clientId: 'jaGPHsP26hXndaxXddaLR4jN2wmqYsZZ',
-  domain: 'dev-ch11m2eo.auth0.com',
-  // redirectUri: 'http://localhost:3000/callback',
-  redirectUri: 'https://measuringchange.com/callback',
+  clientId: process.env.CLIENT_ID,
+  domain: process.env.DOMAIN,
+  redirectUri: process.env.REDIRECT_URI,
 }
